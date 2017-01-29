d447404bbcb666ef6b257a0fcb6678efbad4dbe6
tamura <tamura2004@gmail.com>
2017-01-29 14:02:20 +0900

    Add bfs

--- search.rb
+++ search.rb
@@ -10,12 +10,25 @@ module BlueTokens
   def rotten; [6,4,4,4,4,4,2,2,2,2,2,0,0,0,0,0,0][blue]; end
 end
 
-class State < Struct.new(:infantry,:farmer,:food,:miner,:stone,:science,:happy,:worker,:blue,:yellow)
+class State < Struct.new(:infantry,:farmer,:food,:miner,:stone,:labo,:science,:religion,:worker,:blue,:yellow)
   include YellowTokens
   include BlueTokens
-  attr_accessor :prev_state
-  def initialize; super(1,2,0,2,0,1,0,1,16,18); end
+  attr_accessor :prev_state,:turn
+
+  def initialize(prev_state=nil)
+    if prev_state.kind_of? State
+      super(*prev_state.values)
+      @prev_state = prev_state
+      @turn = prev_state.turn
+    else
+      super(1,2,0,2,0,1,0,0,1,16,18)
+      @turn = 1
+    end
+  end
+
   def growth
+    self.turn += 1
+    self.science += labo
     x = rotten
     if stone >= x
       move(:stone,:blue,x)
@@ -27,16 +40,203 @@ class State < Struct.new(:infantry,:farmer,:food,:miner,:stone,:science,:happy,:
     move(:food,:blue,eat)
     move(:blue,:stone,miner)
   end
+
   def move(from,to,num)
-    x = send(from)
-    y = send(to)
+    x = self[from]
+    y = self[to]
     dif = num > x ? x : num
-    send("#{from}=",x-dif)
-    send("#{to}=",y+dif)
+    self[from] = x - dif
+    self[to] = y + dif
   end
+
   def to_s
-    [:infantry,:farmer,:food,:miner,:stone,:science,:happy,:worker,:blue,:yellow,:rotten,:eat].map do |attr|
-      "#{attr}:#{send(attr)}"
+    [
+      [:turn,"turn"],
+      [:infantry,"兵士"],
+      [:farmer,"農地"],
+      [:food,"食料"],
+      [:miner,"鉱山"],
+      [:stone,"石材"],
+      [:labo,"研究所"],
+      [:science,"科学"],
+      [:religion,"宗教"],
+      [:worker,"労働者"],
+      [:blue,"青"],
+      [:yellow,"黄"]
+    ].map do |attr,label|
+      sprintf("%s:%2d ",label,send(attr))
     end.join(" ")
   end
+
+  def dump
+    if prev_state
+      prev_state.dump
+    end
+    puts to_s
+  end
+
+  def each_worker
+    loop do
+      break unless create_worker
+    end
+    yield clone
+  end
+
+  def create_worker
+    if cost && food >= cost
+      move(:food,:blue,cost)
+      move(:yellow,:worker,1)
+    else
+      false
+    end
+  end
+
+  def each_farmer
+    loop do
+      yield clone
+      break unless create_farmer
+    end
+  end
+
+  def create_farmer
+    if stone >= 2 && worker > 0
+      move(:stone,:blue,2)
+      move(:worker,:farmer,1)
+    else
+      false
+    end
+  end
+
+  def each_miner
+    loop do
+      yield clone
+      break unless create_miner
+    end
+  end
+
+  def create_miner
+    if stone >= 2 && worker > 0
+      move(:stone,:blue,2)
+      move(:worker,:miner,1)
+    else
+      false
+    end
+  end
+
+  def each_religion
+    loop do
+      yield clone
+      break unless create_religion
+    end
+  end
+
+  def create_religion
+    if stone >= 3 && worker > 0
+      move(:stone,:blue,3)
+      move(:worker,:religion,1)
+    else
+      false
+    end
+  end
+
+  def each_infantry
+    loop do
+      yield clone
+      break unless create_infantry
+    end
+  end
+
+  def create_infantry
+    if stone >= 2 && worker > 0
+      move(:stone,:blue,2)
+      move(:worker,:infantry,1)
+    else
+      false
+    end
+  end
+
+  def each_science
+    loop do
+      yield clone
+      break unless create_science
+    end
+  end
+
+  def create_science
+    if stone >= 3 && worker > 0
+      move(:stone,:blue,3)
+      move(:worker,:labo,1)
+    else
+      false
+    end
+  end
+
+  def each_candidate
+    each_worker do |state|
+      state.each_farmer do |state|
+        state.each_miner do |state|
+          state.each_science do |state|
+            state.each_infantry do |state|
+              state.each_religion do |state|
+                if state.satisfy?
+                  state.prev_state = self
+                  yield state
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def satisfy?
+    happy <= worker + religion
+  end
+
 end
+
+class BFS
+  def initialize(state)
+    @queue = []
+    @visited = []
+    @queue << state
+    @visited << state
+  end
+
+  def search
+    loop do
+      if @queue.empty?
+        puts "not found."
+        return
+      else
+        state = @queue.shift
+        if yield state
+          puts "found."
+          state.dump
+          return
+        else
+          state.growth
+          state.each_candidate do |state|
+            unless @visited.include? state
+              @visited << state
+              @queue << state
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+state = State.new
+bfs = BFS.new(state)
+bfs.search do |state|
+  state.science == 12 && state.infantry == 3
+end
+
+
+
+
+
+
--- test_search.rb
+++ test_search.rb
@@ -1,5 +1,45 @@
 require "./search.rb"
 
+class TestYellowTokens < Test::Unit::TestCase
+  def test_eat
+    state = State.new
+    assert_equal 0, state.eat
+    state.yellow = 1
+    assert_equal 4, state.eat
+    state.yellow = 0
+    assert_equal 6, state.eat
+  end
+
+  def test_cost
+    state = State.new
+    assert_equal 2, state.cost
+    state.yellow = 1
+    assert_equal 7, state.cost
+    state.yellow = 0
+    assert_equal nil, state.cost
+  end
+
+  def test_happy
+    state = State.new
+    assert_equal 0, state.happy
+    state.yellow = 1
+    assert_equal 7, state.happy
+    state.yellow = 0
+    assert_equal 8, state.happy
+  end
+end
+
+class TestBlueTokens < Test::Unit::TestCase
+  def test_rotten
+    state = State.new
+    assert_equal 0, state.rotten
+    state.blue = 1
+    assert_equal 4, state.rotten
+    state.blue = 0
+    assert_equal 6, state.rotten
+  end
+end
+
 class TestState < Test::Unit::TestCase
   def test_initialize
     state = State.new
@@ -8,13 +48,21 @@ class TestState < Test::Unit::TestCase
     assert_equal 0,state.food
     assert_equal 2,state.miner
     assert_equal 0,state.stone
-    assert_equal 1,state.science
-    assert_equal 0,state.happy
+    assert_equal 1,state.labo
+    assert_equal 0,state.religion
     assert_equal 1,state.worker
     assert_equal 16,state.blue
     assert_equal 18,state.yellow
-  end
+    assert_equal nil, state.prev_state
 
+    state.move(:blue,:food,10)
+    state_2 = State[state]
+    state_2.move(:food,:blue,5)
+
+    assert_equal 10,state.food
+    assert_equal 5, state_2.food
+    assert_equal state,state_2.prev_state
+  end
   def test_growth
     state = State.new
     7.times{state.growth}
@@ -22,6 +70,11 @@ class TestState < Test::Unit::TestCase
     assert_equal state.stone, 2
     assert_equal state.blue, 4
     assert_equal state.rotten, 4
-
+  end
+  def test_move
+    state = State.new
+    state.move(:yellow,:farmer,10)
+    assert_equal 8, state.yellow
+    assert_equal 12, state.farmer
   end
 end
