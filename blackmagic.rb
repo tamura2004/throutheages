d8ad9bf50859f4e22bcff98e982a282905c4b878
tamura <tamura2004@gmail.com>
2017-01-31 02:45:54 +0900

    fire

--- blackmagic.rb
+++ blackmagic.rb
@@ -1,21 +1,21 @@
 class State < Struct.new(:x,:y)
-  def fiber_x
-    Fiber.new do
-      Fiber.yield 
-      10.times do |n|
-        self.x = n
-        Fiber.yield self
-      end
-    end
+  def eachx
+    3.times.map{|n|State[x+n,y]}
   end
 
-  def fiber_y
-    Fiber.new do
-      10.times do |n|
-        self.y = n
-        Fiber.yield self
-      end
-    end
+  def eachy
+    3.times.map{|n|State[x,y+n]}
   end
 end
+
+state = State[10,20]
+
+[state]
+  .lazy
+  .flat_map(&:eachx)
+  .flat_map(&:eachy)
+  .take(4)
+  .each do |state|
+    p state
+  end
 
